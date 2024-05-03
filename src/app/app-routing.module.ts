e8c8fb91af2345abca9f9c68681d033cda8afc26
Matheus Luiz <matluiz78@gmail.com>
2024-05-03 12:40:46 -0300

    roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/add', component: ProductAddComponent },
+  { path: 'products/:id/edit', component: ProductEditComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
